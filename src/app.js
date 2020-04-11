b11e87aee349480a240d7e52f25fa523fee958b2
rafael399 <rafael399@gmail.com>
2020-04-11 15:52:05 -0300

    desafio finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(r => r.id === id);
+  
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository with the given ID does not exist' });
+  }
+
+  const repo = repositories[repoIndex];
+
+  repo.title = title;
+  repo.url = url;
+  repo.techs = techs;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(r => r.id === id);
+  
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository with the given ID does not exist' });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  // A rota deve aumentar o número de likes do repositório específico escolhido através do id presente nos parâmetros da rota, a cada chamada dessa rota, o número de likes deve ser aumentado em 1;
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(r => r.id === id);
+  
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository with the given ID does not exist' });
+  }
+
+  const repo = repositories[repoIndex];
+
+  repo.likes++;
+
+  return response.json(repo);
 });
 
 module.exports = app;
